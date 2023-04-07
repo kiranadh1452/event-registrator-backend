d23cb86f8758f4fbd5b7330ea461f74fa1503bf7
Kiran Adhikari <kiranadh1452@gmail.com>
2023-04-07 20:46:42 +0545

    Added proper routes for event types

--- src/v1/event-types/router.js
+++ src/v1/event-types/router.js
@@ -1,11 +1,15 @@
 import express from "express";
 
-// Import the event controller here
+// Import the event type middleware here
+import * as eventTypesMiddleware from "./middleware.js";
 
-const router = express.Router();
+// Import the event type controller here
+import * as eventTypesController from "./controller.js";
+
+const eventTypeRouter = express.Router();
 
 // Define the event routes here
-router.get("/test", (req, res) => {
+eventTypeRouter.get("/test", (req, res) => {
     return res.status(200).json({
         code: 200,
         message: "Success",
@@ -13,4 +17,26 @@ router.get("/test", (req, res) => {
     });
 });
 
-export default router;
+eventTypeRouter.use(
+    eventTypesMiddleware.userAuthenticatorForEventTypesMiddleware
+);
+
+// Get a list of all event types
+eventTypeRouter.get("/", eventTypesController.getAllEventTypesController);
+
+// Create a new event type
+eventTypeRouter.post("/", eventTypesController.createEventTypeController);
+
+// Get the details of a specific event type by ID
+eventTypeRouter.get("/:id", eventTypesController.getEventTypeByIdController);
+
+// Update an existing event type by ID
+eventTypeRouter.put("/:id", eventTypesController.updateEventTypeByIdController);
+
+// Delete an event type by ID
+eventTypeRouter.delete(
+    "/:id",
+    eventTypesController.deleteEventTypeByIdController
+);
+
+export default eventTypeRouter;
