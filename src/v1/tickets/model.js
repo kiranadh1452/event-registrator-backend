73d08630ab3a2597768e2c459f6729ea032cec45
Kiran Adhikari <kiranadh1452@gmail.com>
2023-04-10 00:02:39 +0545

    Updated the ticket model to include more data in it

--- src/v1/tickets/model.js
+++ src/v1/tickets/model.js
@@ -1,43 +1,74 @@
 import mongoose from "mongoose";
 
-const ticketSchema = new mongoose.Schema(
-    {
-        userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        eventId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Event",
-            required: true,
-        },
-        price: {
-            type: Number,
-            required: true,
-        },
-        quantity: {
-            type: Number,
-            required: true,
-        },
-        status: {
-            type: String,
-            enum: ["pending", "confirmed", "cancelled"],
-            default: "pending",
-        },
-        type: {
-            type: String,
-            required: true,
-        },
-        invoiceId: {
-            type: String,
-        },
+const TicketSchema = new mongoose.Schema({
+    eventId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Event",
+        required: true,
     },
-    {
-        timestamps: true,
-    }
-);
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ["created", "opened", "expired", "canceled", "completed"],
+        default: "created",
+    },
+    sessionId: {
+        type: String,
+        index: true,
+    },
+    total_amount: {
+        type: Number,
+    },
+    session_created: {
+        type: Date,
+    },
+    currency: {
+        type: String,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    priceId: {
+        type: String,
+        index: true,
+    },
+    payment_intent: {
+        type: String,
+    },
+    payment_status: {
+        type: String,
+        enum: ["succeeded", "failed", "canceled"],
+    },
+    session_url: {
+        type: String,
+    },
+    amount_shipping: {
+        type: Number,
+    },
+    amount_discount: {
+        type: Number,
+    },
+    amount_tax: {
+        type: Number,
+    },
+});
 
-const Ticket = mongoose.model("Ticket", ticketSchema);
-
+const Ticket = mongoose.model("Ticket", TicketSchema);
 export default Ticket;
