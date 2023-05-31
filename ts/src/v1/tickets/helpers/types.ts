66c19a76dd05d5429f88857c4aaa5eecf1b1c624
Kiran Adhikari <kiranadh1452@gmail.com>
2023-05-31 21:25:33 +0545

    feat: Add mongoose model for ticket

---
+++ ts/src/v1/tickets/helpers/types.ts
@@ -0,0 +1,33 @@
+import { Document, Model, Schema } from "mongoose";
+
+export interface ITicket extends Document {
+    _id: string;
+    eventId: Schema.Types.ObjectId;
+    quantity?: number;
+    type?: string;
+    status?: "open" | "complete" | "expired";
+    sessionId: string;
+    totalAmount?: number;
+    sessionCreated?: Date;
+    currency?: string;
+    userId: string; // we are using the firebase id, so not an objectId here
+    priceId: string;
+    paymentIntent?: string;
+    paymentStatus?: "paid" | "unpaid" | "no_payment_required";
+    sessionUrl?: string;
+    amountShipping?: number;
+    amountDiscount?: number;
+    amountTax?: number;
+    created_at?: Date;
+    updated_at?: Date;
+    updateTicket: (
+        this: ITicket,
+        updateProps: Partial<ITicket>
+    ) => Promise<ITicket>;
+}
+
+export interface ITicketModel extends Model<ITicket> {
+    fetchTickets(queryParams: any): Promise<ITicket[]>;
+    createTicket(ticketData: any): Promise<ITicket>;
+    deleteTicket(ticketId: string): Promise<ITicket>;
+}
---
+++ ts/src/v1/tickets/model.ts
@@ -0,0 +1,126 @@
+import mongoose, { Schema } from "mongoose";
+
+// importing types
+import { ITicket, ITicketModel } from "./helpers/types";
+
+export const ticketSchema: Schema<ITicket> = new Schema<ITicket>({
+    eventId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Event",
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        default: 1,
+    },
+    type: {
+        type: String,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ["open", "complete", "expired"],
+        default: "open",
+    },
+    sessionId: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    totalAmount: {
+        type: Number,
+    },
+    sessionCreated: {
+        type: Date,
+    },
+    currency: {
+        type: String,
+    },
+    userId: {
+        type: String,
+        ref: "User",
+        required: true,
+        index: true,
+    },
+    priceId: {
+        type: String,
+        index: true,
+        required: true,
+    },
+    paymentIntent: {
+        type: String,
+    },
+    paymentStatus: {
+        type: String,
+        enum: ["paid", "unpaid", "no_payment_required"],
+        required: true,
+        default: "unpaid",
+    },
+    sessionUrl: {
+        type: String,
+    },
+    amountShipping: {
+        type: Number,
+    },
+    amountDiscount: {
+        type: Number,
+    },
+    amountTax: {
+        type: Number,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+/**
+ * @Hooks for ticketSchema
+ */
+ticketSchema.pre<ITicket>("save", function (next) {
+    this.updated_at = new Date();
+    next();
+});
+
+// Methods for ticketSchema
+ticketSchema.methods.updateTicket = async function (
+    this: ITicket,
+    updateProps: Partial<ITicket>
+): Promise<ITicket> {
+    //TODO: implement this
+    return this;
+};
+
+/**
+ * @Static_Methods
+ */
+ticketSchema.statics.fetchTickets = async function (
+    queryParams: any
+): Promise<ITicket[]> {
+    // TODO: Implement this
+    const tickets: ITicket[] = []; // = await this.find();
+
+    return tickets;
+};
+
+ticketSchema.statics.createTicket = async function (
+    ticketData: any
+): Promise<ITicket> {
+    // TODO: Implement this
+    return ticketData as ITicket;
+};
+
+ticketSchema.statics.deleteTicket = async function (
+    ticketId: string
+): Promise<boolean> {
+    // TODO: Implement this
+    return false;
+};
+
+const Ticket = mongoose.model<ITicket, ITicketModel>("Ticket", ticketSchema);
+
+export default Ticket;
